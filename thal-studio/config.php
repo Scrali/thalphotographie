6eb175e034c0a4e0d59c1a3542cd3ba74def7597
Scrali <140433279+Scrali@users.noreply.github.com>
2026-07-02 16:48:18 +0200

    Add files via upload

---
+++ thal-studio/config.php
@@ -0,0 +1,10 @@
+<?php
+// Identifiants THAL Studio.
+// IMPORTANT : ce ne sont PAS tes identifiants OVH.
+// Change le nom d'utilisateur et le hash du mot de passe avant mise en ligne.
+
+define('THAL_USER', 'jonathan');
+
+// Mot de passe temporaire : ChangeMoi!2026
+// À remplacer avec le hash généré par generate_hash.php.
+define('THAL_PASSWORD_HASH', '$2y$10$S.Wj0YyGZIT.e0G7U84bmuOot3b8zyqXebnIlVDjuc5mbDRmjC/92');
