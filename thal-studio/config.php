1954497deab8d0749ff0c08374e7981ce331db4f
Scrali <140433279+Scrali@users.noreply.github.com>
2026-07-02 18:38:34 +0200

    Delete thal-studio directory

--- thal-studio/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('THAL_USER', 'jonathan');
-
-define('THAL_PASSWORD_HASH', '$2y$12$th49iFlWTjYGaklmXmTDIOLFBWnbfgO0EbWywQ4Cyt/fqKhyI9WTm');
--- thal-studio/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (!empty($_SESSION['thal_logged_in'])) {
-    header('Location: app.php');
-} else {
-    header('Location: login.php');
-}
-exit;
