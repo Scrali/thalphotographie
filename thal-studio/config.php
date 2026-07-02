dbb088882d1d35b08988af53eae22819872dac3b
Scrali <140433279+Scrali@users.noreply.github.com>
2026-07-02 18:21:31 +0200

    Update config.php

--- thal-studio/config.php
+++ thal-studio/config.php
@@ -2,4 +2,4 @@
 
 define('THAL_USER', 'jonathan');
 
-define('THAL_PASSWORD_HASH', '$2y$12$VGTeSd3v00slhOHyZ2LQsOque1CM3HlLG/0WHB4J2l6n.C0JidKpe');
+define('THAL_PASSWORD_HASH', '$2y$12$th49iFlWTjYGaklmXmTDIOLFBWnbfgO0EbWywQ4Cyt/fqKhyI9WTm');
