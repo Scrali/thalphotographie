8d3acdea9a43587c4e681a2cc814f14155786152
Scrali <140433279+Scrali@users.noreply.github.com>
2026-07-07 22:54:02 +0200

    Update config.php

--- thal-studio/config.php
+++ thal-studio/config.php
@@ -1,14 +1,9 @@
 <?php
-// Configuration THAL Studio.
-// Ces identifiants ne sont PAS ceux d'OVH.
+// Configuration THAL Studio
 
 define('THAL_USER', 'jonathan');
 
-// Mot de passe temporaire : ChangeMoi!2026
-// Change-le depuis Paramètres > Changer le mot de ;
-
-<?php
-
-define('THAL_USER', 'jonathan');
-
-define('THAL_PASSWORD_HASH', '$2y$10$P.xQ4k0D2dz6Q.KMB8jDIuhxbVX1wUe7QrG/7J7fwIksFv3Q0MMeu');
+define(
+    'THAL_PASSWORD_HASH',
+    '$2y$10$P.xQ4k0D2dz6Q.KMB8jDIuhxbVX1wUe7QrG/7J7fwIksFv3Q0MMeu'
+);
