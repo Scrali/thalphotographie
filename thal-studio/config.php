47aa4db9855c81af14f2e0e8d5d4db6c17e19122
Scrali <140433279+Scrali@users.noreply.github.com>
2026-07-07 22:48:37 +0200

    Update config.php

--- thal-studio/config.php
+++ thal-studio/config.php
@@ -5,5 +5,6 @@
 define('THAL_USER', 'jonathan');
 
 // Mot de passe temporaire : ChangeMoi!2026
-// Change-le depuis Paramètres > Changer le mot de passe.
-define('THAL_PASSWORD_HASH', '$2y$12$57VTIVpY53IZYBsB6XV9iepya9XGpXzRwvdyoxbzpP5KSxo9O2EsC');
+// Change-le depuis Paramètres > Changer le mot de ;
+define('THAL_USER', 'jonathan');
+define('THAL_PASSWORD', 'jHi@2015');
